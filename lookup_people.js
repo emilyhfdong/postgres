66ebfe3878751e98f2fcf51a54eb8e87f57d1c3c
Emily Dong <emilyhfdong@gmail.com>
2018-06-26 16:07:04 +0000

    added lookup_people.js

---
+++ lookup_people.js
@@ -0,0 +1,64 @@
+// require pg driver and settings
+const pg = require("pg");
+const settings = require("./settings"); // settings.json
+
+// set the command line input as searchName
+const searchName = process.argv[2];
+
+// make new client using the settings set in the settings.json file
+const client = new pg.Client({
+  user     : settings.user,
+  password : settings.password,
+  database : settings.database,
+  host     : settings.hostname,
+  port     : settings.port,
+  ssl      : settings.ssl
+});
+
+// function that takes a date object and returns a string in the format YYYY-MM-DD
+function returnDateStr (date) {
+  let year = date.getFullYear();
+  let month = date.getMonth() + 1;
+  let day = date.getDate();
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  return `${year}-${month}-${day}`;
+}
+
+// function to print the formatted search results (includes number of results, full name and birthdate)
+function printSearchResults (result) {
+  console.log(`Found ${result.rowCount} by the name ${searchName}`);
+  for (let i = 0; i < result.rowCount; i ++) {
+    console.log(`- ${i + 1}: ${result.rows[i].first_name} ${result.rows[i].last_name}, born ${returnDateStr(result.rows[i].birthdate)}`)
+  }
+}
+
+// function that makes the query based on a name and runs the printSearchResults function
+function searchQuery (name) {
+  console.log("Searching ...");
+  client.query(`
+    SELECT * FROM famous_people
+      WHERE first_name LIKE '${name}'
+        OR last_name LIKE '${name}'`, (err, result) => {
+    if (err) {
+      return console.error("error running query", err);
+    }
+    printSearchResults(result);
+    client.end();
+  });
+}
+
+// connect to the database and runs searchQuery function
+client.connect((err) => {
+  if (err) {
+    return console.error("Connection Error", err);
+  }
+  searchQuery(searchName)
+});
+
+
+
